f6e1174a2f73562fff6dcf0f3de8ebe3f75a99e9
KAMONPAN KAEWHEANG <kamonpan@dit.daikin.co.jp>
2021-11-10 15:59:59 +0700

    Initial Commit

---
+++ AR AirCon Design/AR_AirCon_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_AirCon_DesignApp.swift
+//  AR AirCon Design
+//
+//  Created by admin on 10/11/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct AR_AirCon_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AR AirCon Design/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AR AirCon Design
+//
+//  Created by admin on 10/11/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
